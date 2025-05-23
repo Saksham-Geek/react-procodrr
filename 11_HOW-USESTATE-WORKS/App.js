956323798179c3283bc44833c80def1de4c25454
Saksham <pethkarsaksham@gmail.com>
2025-05-23 16:29:39 +0530

    11 HOW USESTATE WORKS INTERNALLY

---
+++ 11_HOW-USESTATE-WORKS/App.js
@@ -0,0 +1,13 @@
+import AppleCounter from "./components/AppleCounter"
+import Counter from "./components/Counter"
+
+
+function App() {
+  return (
+  // <AppleCounter />
+  <Counter/>
+
+)
+}
+
+export default App
